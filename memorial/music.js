bb9a3a615dda31ea8f66e777276ca9b2da0a0c53
FortNbreak <98566516+FortNbreak@users.noreply.github.com>
2024-08-22 23:21:43 -0400

    Delete memorial directory

--- memorial/music.js
+++
@@ -1,8 +0,0 @@
-const music = new Audio("music.mp3");
-const bodyElement = document.body;
-var likeButton = document.getElementById('likeButton');
-likeButton.addEventListener('click', function() {
-    music.play()
-    bodyElement.classList.toggle("fire-gradient");
-});
-
